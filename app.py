754f77951242a8114d6a30451c1d614fd564b339
hanialfii <alaikaalfih@gmail.com>
2025-08-14 14:36:34 +0700

    Add welcome message endpoint to quotes API

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/quotes")
 def get_quotes():
     return {"quotes": quotes()}
+@app.get("/")
+def slsash():
+    return {"message": "Welcome to the Quotes API. Use /quotes to get quotes."}
+    
